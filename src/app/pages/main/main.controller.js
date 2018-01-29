b08d8c5dbed19b3c551c153e05e6fadac8221c43
ROKITTECHUA\mykhaylo.kozyar <mykhaylo.kozya@io-tahoe.com>
2018-01-29 16:49:00 +0200

    left-side

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -10,9 +10,22 @@ angular
   .module('chat')
   .controller('mainController', mainController);
 
-  mainController.$inject = ['$rootScope', '$scope', '$state'];
+mainController.$inject = ['$rootScope', '$scope', '$state', '$window'];
 
 
 function mainController($rootScope, $scope, $state) {
- 
+console.log($(window).width())
+  $scope.screenWidthF = function(event) {
+    $scope.screenWidth = event.target.innerWidth;
+  }
+  $scope.leftBarVisible = true
+
+  $scope.toggleLeftBar = function () {
+    $scope.leftBarVisible = !$scope.leftBarVisible
+  }
+
+  $scope.$watch('screenWidth', function (n, o) {
+    console.log($scope.screenWidth)
+  })
+
 }
