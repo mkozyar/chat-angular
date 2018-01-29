da2ba07b2b6e4724657b9387539913cb3ed5faea
ROKITTECHUA\mykhaylo.kozyar <mykhaylo.kozya@io-tahoe.com>
2018-01-29 18:44:37 +0200

    let bar response

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -10,22 +10,74 @@ angular
   .module('chat')
   .controller('mainController', mainController);
 
-mainController.$inject = ['$rootScope', '$scope', '$state', '$window'];
+mainController.$inject = ['$rootScope', '$scope', '$state', '$window', '$timeout'];
 
 
-function mainController($rootScope, $scope, $state) {
-console.log($(window).width())
-  $scope.screenWidthF = function(event) {
-    $scope.screenWidth = event.target.innerWidth;
-  }
+
+function mainController($rootScope, $scope, $state, $window, $timeout) {
+
+
+  $scope.screenWidth = window.innerWidth;
+
+  window.addEventListener("resize", windowResize);
+
   $scope.leftBarVisible = true
+  $scope.screenWidth = window.innerWidth;
+
+  function windowResize() {
+    setTimeout(function(){
+   
+    $scope.screenWidth = window.innerWidth;
+    $state.reload()
+  })
+
+  }
+
+
+
+
+  $scope.fullLeftSideWidth = function () {
+    if ($scope.leftBarVisible && $scope.screenWidth < 900) {
+      return 'null-width'
+    }
+    if ($scope.leftBarVisible && $scope.screenWidth > 900) {
+      return 'messages-area'
+    } 
+    else
+      return 'messages-area full-width'
+  }
+
+  $scope.leftSideClass = function () {
+    if ($scope.leftBarVisible && $scope.screenWidth > 900) {
+      return 'left-side'
+
+    }
+    if ($scope.leftBarVisible && $scope.screenWidth < 900) {
+      return 'full-width'
+
+    }
+    else {
+      return 'small'
+
+    }
+  }
 
   $scope.toggleLeftBar = function () {
     $scope.leftBarVisible = !$scope.leftBarVisible
   }
 
   $scope.$watch('screenWidth', function (n, o) {
-    console.log($scope.screenWidth)
+    if ($scope.screenWidth < 900) {
+      $scope.leftBarVisible = false
+    }
+    else {
+      $scope.leftBarVisible = true
+    }
+  })
+
+  $scope.$watch('leftBarVisible', function (n, o) {
+    
+    
   })
 
 }
