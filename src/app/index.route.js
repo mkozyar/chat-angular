5984a6cbafa0d491b7c80f9a6ac6a350c43b54cb
ROKITTECHUA\mykhaylo.kozyar <mykhaylo.kozya@io-tahoe.com>
2018-02-07 00:55:31 +0200

    auth

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -9,17 +9,21 @@
     function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
 
         $stateProvider
-
         .state('home', {
             url: '/',
-           
                 templateUrl: 'app/pages/main/main.html',
                 controller: 'mainController'
               
-            
-            
           })
-
+          .state('home.chat', {
+            url: 'chat',
+            views: {
+              '@home': {
+                templateUrl: 'app/pages/chat/chat.html',
+                controller: 'chatController'
+              }
+            }
+          })
 
 
         $urlRouterProvider.otherwise('/');
---
+++ src/app/pages/chat/chat.controller.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * chatController
+ *
+ * Description
+ */
+
+angular
+  .module('chat')
+  .controller('chatController', chatController);
+
+chatController.$inject = ['$rootScope', '$scope', '$state', '$window', '$timeout'];
+
+
+
+function chatController($rootScope, $scope, $state, $window, $timeout) {
+
+alert(1212)  
+  $scope.screenWidth = window.innerWidth;
+
+  window.addEventListener("resize", windowResize);
+
+  $scope.leftBarVisible = true
+  $scope.screenWidth = window.innerWidth;
+
+  function windowResize() {
+    setTimeout(function () {
+
+      $scope.screenWidth = window.innerWidth;
+      $state.reload()
+    })
+
+  }
+
+
+
+
+  $scope.fullLeftSideWidth = function () {
+    if ($scope.leftBarVisible && $scope.screenWidth < 700) {
+      return 'display-none'
+    }
+    if ($scope.leftBarVisible && $scope.screenWidth > 700) {
+      return 'messages-area'
+    }
+    else
+      return 'messages-area full-width'
+  }
+
+  $scope.leftSideClass = function () {
+    if ($scope.leftBarVisible && $scope.screenWidth > 700) {
+      return 'left-side'
+
+    }
+    if ($scope.leftBarVisible && $scope.screenWidth < 700) {
+      return 'left-side-full-width'
+
+    }
+    else {
+      return 'small'
+
+    }
+  }
+
+  $scope.toggleLeftBar = function () {
+    $scope.leftBarVisible = !$scope.leftBarVisible
+  }
+
+  $scope.$watch('screenWidth', function (n, o) {
+    if ($scope.screenWidth < 700) {
+      $scope.leftBarVisible = false
+    }
+    else {
+      $scope.leftBarVisible = true
+    }
+  })
+
+  $scope.scrollTop = $('div.date:last').offset().top + 10000
+
+  $(document).ready(function () {
+    $('.messages-list').animate({ "scrollTop": $scope.scrollTop },1);
+  });
+
+  $scope.$watch('leftBarVisible', function (n, o) {
+
+
+  })
+
+}
